30e7fb2580f24effeef10c626b6f5776184561fd
aasthavashishtha <aastha.vashishtha.2003@gmail.com>
2024-05-16 00:03:45 +0530

    15-05-24

--- 48-rotateImage.cpp
+++ 48-rotateImage.cpp
@@ -1,17 +1,29 @@
 void rotate(int** matrix, int matrixSize, int* matrixColSize) {
-    int i=0,j=0;
-    int n = *matrixColSize;
-    int dupe[n][n];
-    for(i=0;i<n;i++)
-    {
-        for(j=n-1;j>=0;j--)
-        {
-            dupe[i][n-1-j]=matrix[j][i];
+    // int i=0,j=0;
+    // int n = *matrixColSize;
+    // int dupe[n][n];
+    // for(i=0;i<n;i++)
+    // {
+    //     for(j=n-1;j>=0;j--)                      //Brute force approach
+    //     {
+    //         dupe[i][n-1-j]=matrix[j][i];
+    //     }
+    // }
+    // for(i=0;i<n;i++)
+    // {
+    //     for(j=0;j<n;j++)
+    //         matrix[i][j]=dupe[i][j];
+    // }
+
+    void rotate(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        for(int i=0;i<row; i++){
+            for(int j=0; j<=i;j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
         }
-    }
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n;j++)
-            matrix[i][j]=dupe[i][j];
-    }
+        for(int i=0;i<row;i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    
 }
